4f44de83e879d7f4ceb3f8eff7235b6edae38bfc
thing1 <thing1@seacrossedlovers.xyz>
2024-07-11 13:34:51 +0100

    fixed a bug that caused a new line to crash the program

--- chart.c
+++ chart.c
@@ -29,7 +29,7 @@ line *putinlines(FILE *f){
 	char *tok;
 	size_t i = 0;
 	
-	if (fgets(buffer, 256, f) == NULL){
+	if (fgets(buffer, 256, f) == NULL || buffer[0] == '\n'){
 		free(newline);
 		free(buffer);
 		return NULL;
